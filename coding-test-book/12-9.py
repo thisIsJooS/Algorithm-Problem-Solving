4b8554807836c430accc92ae3e689a125623a37d
joos <thisis.joos@gmail.com>
2021-12-12 22:45:19 +0000

    upload files

--- coding-test-book/12-9.py
+++ coding-test-book/12-9.py
@@ -3,20 +3,23 @@
 
 def solution(s):
     answer = len(s)
-    for step in range(1, len(s)//2 + 1):
+    
+    for step in range(1, len(s)//2+1):
+        prev = s[:step]
         compressed = ""
-        prev = s[0:step]
-        count = 1
+        cnt = 1
         
         for j in range(step, len(s), step):
-            if prev == s[j:j+step]:
-                count += 1
+            now = s[j:j+step]
+            if prev == now:
+                cnt += 1
+            
             else:
-                compressed += (str(count)+prev) if count >= 2 else prev
-                prev = s[j:j+step]
-                count = 1
-        
-        compressed += str(count) + prev if count >= 2 else prev
+                compressed += str(cnt)+prev if cnt >=2 else prev
+                prev = now
+                cnt = 1
+    
+        compressed += str(cnt)+prev if cnt >=2 else prev
         answer = min(answer, len(compressed))
     
     return answer
