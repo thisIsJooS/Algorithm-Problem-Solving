8b3d8d0a2630221a9c8bc4501c12f76f3028d4a8
joos <thisis.joos@gmail.com>
2021-12-16 22:55:45 +0000

    upload files

---
+++ coding-test-book/13-16.py
@@ -0,0 +1,61 @@
+# 341p [연구소] - 삼성전자 SW 역량테스트
+# https://www.acmicpc.net/problem/14502
+
+from itertools import combinations
+from collections import deque
+import copy
+
+n, m = map(int, input().split())
+arr = []
+room_locations = []
+virus_locations = []
+res = 0      
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+    for j in range(m):
+        if arr[i][j] == 0:
+            room_locations.append((i, j))
+        if arr[i][j] == 2:
+            virus_locations.append((i, j))    
+        
+    
+
+# 주어진 맵에 대하여 전부 감염시킨다 - bfs
+def infect_all():
+    q = []
+    q = copy.deepcopy(virus_locations)
+    while q:
+        x, y = q.pop()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 0:
+                arr[nx][ny] = 2
+                q.append((nx, ny))
+        
+
+for candidate in list(combinations(room_locations, 3)):
+    cnt = 0 
+    for x, y in candidate:
+        arr[x][y] = 1
+        
+    infect_all()
+    
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 0:
+                cnt += 1
+    
+    res = max(res, cnt)
+    
+    for x, y in room_locations:
+        arr[x][y] = 0
+
+
+print(res)
+
