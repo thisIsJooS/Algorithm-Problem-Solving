e746cbdcd3ca29f3a06a1c7da89a3a0ea19af147
joos <thisis.joos@gmail.com>
2021-12-16 23:04:31 +0000

    upload files

--- coding-test-book/13-16_dfs.py
+++ coding-test-book/13-16_dfs.py
@@ -2,8 +2,6 @@
 # https://www.acmicpc.net/problem/14502
 
 from itertools import combinations
-from collections import deque
-import copy
 
 n, m = map(int, input().split())
 arr = []
@@ -21,30 +19,27 @@ for i in range(n):
         if arr[i][j] == 2:
             virus_locations.append((i, j))    
         
-    
 
-# 주어진 맵에 대하여 전부 감염시킨다 - bfs
-def infect_all():
-    q = []
-    q = copy.deepcopy(virus_locations)
-    while q:
-        x, y = q.pop()
+# 주어진 맵에 대하여 전부 감염시킨다 - dfs
+def infect(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 0:
-                arr[nx][ny] = 2
-                q.append((nx, ny))
-        
+        if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 0:
+            arr[nx][ny] = 2
+            infect(nx, ny)
 
+        
 for candidate in list(combinations(room_locations, 3)):
     cnt = 0 
+    # 3개를 골라 벽을 세운다
     for x, y in candidate:
         arr[x][y] = 1
-        
-    infect_all()
+    
+    # 기존 바이러스 위치에서 dfs로 감염시킨다
+    for x, y in virus_locations:
+        infect(x, y)
     
     for i in range(n):
         for j in range(m):
@@ -53,6 +48,7 @@ for candidate in list(combinations(room_locations, 3)):
     
     res = max(res, cnt)
     
+    # 처음 배열로 복귀시킨다
     for x, y in room_locations:
         arr[x][y] = 0
 
