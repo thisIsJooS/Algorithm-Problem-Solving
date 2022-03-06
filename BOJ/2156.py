d9f865bdf9a69ce839e533b5776eba4fca035a14
thisIsJooS <thisis.joos@gmail.com>
2022-03-06 10:22:06 +0900

    upload 2156.py

--- BOJ/2156.py
+++ BOJ/2156.py
@@ -1,1 +1,21 @@
-dffdf
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    arr = [0] + [int(input()) for _ in range(n)]
+    if n==1:
+        return arr[n]
+    if n==2:
+        return sum(arr)
+    
+    dp = [0] * (n+1)
+    dp[1] = arr[1]
+    dp[2] = arr[1]+arr[2]
+    
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-1], dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i])
+        
+    return dp[n]
+
+print(solve())
